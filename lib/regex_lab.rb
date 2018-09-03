76c244226b948b540c8c1e8a9790615ca9adf547
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-03 18:09:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,9 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiou]/i)
-    return true
-  else
-    return false
-  end
+  word.match(/\A[aeiou]/i) ? true : false 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing/)
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
@@ -15,11 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z].+[.?!]\z/)
-    return true
-  else
-    return false
-  end
+  text.match(/\A[A-Z].+[.?!]\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
